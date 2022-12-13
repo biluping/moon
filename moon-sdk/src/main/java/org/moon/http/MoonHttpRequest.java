9aee42cfd734c9a1368deb112c9decd4e6b652c7
rabbit <2631132715@qq.com>
2022-12-13 23:28:34 +0800

    refactor: 去除自己封装的工具类，使用hutool工具类

--- moon-sdk/src/main/java/org/moon/http/MoonHttpRequest.java
+++ moon-sdk/src/main/java/org/moon/http/MoonHttpRequest.java
@@ -1,5 +1,6 @@
 package org.moon.http;
 
+import cn.hutool.http.HttpUtil;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.TypeReference;
@@ -8,9 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.moon.entity.vo.BaseVo;
 import org.moon.entity.vo.MoonConfigVo;
 import org.moon.exception.MoonBadRequestException;
-import org.moon.utils.HttpUtils;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,15 +31,15 @@ public class MoonHttpRequest {
     public static List<MoonConfigVo> getPublishConfig() {
         String url = String.format("%s/config/moon/%s?isPublish=1", serverUrl, appid);
         try {
-            BaseVo<List<MoonConfigVo>> vo = HttpUtils.doGet(url, new TypeReference<>() {});
+            String json = HttpUtil.get(url, 5000);
+            BaseVo<List<MoonConfigVo>> vo = JSONObject.parseObject(json, new TypeReference<>(){});
             if (vo.getCode() == 200){
-                log.info("获取moon配置成功, 数据:");
-                System.out.println(JSONObject.toJSONString(vo.getData(), SerializerFeature.PrettyFormat));
+                log.info("获取moon配置成功, 数据:\n{}", JSONObject.toJSONString(vo.getData(), SerializerFeature.PrettyFormat));
                 return vo.getData();
             } else {
                 throw new MoonBadRequestException(JSON.toJSONString(vo));
             }
-        } catch (IOException e) {
+        } catch (Exception e) {
             log.error("请求异常, url: {}", url, e);
             return new ArrayList<>();
         }
